a6246c091120fc83268b9a653097d151734e6759
Sidharth <sidmuralidharan@gmail.com>
2023-03-15 13:07:26 +0530

    task files

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1";
+import { utf8ToBytes } from "ethereum-cryptography/utils";
+import { keccak256 } from "ethereum-cryptography/keccak";
 
-function Transfer({ address, setBalance }) {
+function Transfer({ address, setBalance, privateKey }) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
 
@@ -9,15 +12,16 @@ function Transfer({ address, setBalance }) {
 
   async function transfer(evt) {
     evt.preventDefault();
+    const data = { sender: address, recipient, amount: parseInt(sendAmount)};
+    const bytes = utf8ToBytes(JSON.stringify(data));
+    const hash = keccak256(bytes);
+    const signature = await secp.sign(hash, privateKey, { recovered: true });
+    var sign = Array.from(signature[0])
 
     try {
       const {
         data: { balance },
-      } = await server.post(`send`, {
-        sender: address,
-        amount: parseInt(sendAmount),
-        recipient,
-      });
+      } = await server.post(`send`, {...data, signature: sign, recovery: signature[1]});
       setBalance(balance);
     } catch (ex) {
       alert(ex.response.data.message);
--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const secp = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const {toHex, utf8ToBytes} = require("ethereum-cryptography/utils");
 const port = 3042;
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0441e0afeaf16ca31d0e0844a3616ec2f9e137ec80d1ab64fe28bec7eac04444ecc359147b45486dd91ce553ecd406e221258c36b325ef9d9dc22fc2a4d34de105": 1000,
+  "04bee60c99886bfc2a49647f61bf71dd15ff9b7a545b5c8d021255a797840a4913ae8120676aeecf47bb0ee9f17debf1ac8cf16a15fdc9a1a5c42ecb299923b5c0": 500,
+  "04e5ae9c2fee29cf83a892025f0becd3e817f4458f15ae124a10d944a5907fbd86385d85cde9674d833b7b2b8a7cb16cd60b23285cff5ba61cc6bb9e697f7bf542": 750,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -18,18 +21,47 @@ app.get("/balance/:address", (req, res) => {
   res.send({ balance });
 });
 
-app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+app.post("/send", async (req, res) => {
+  const { sender, recipient, amount, signature, recovery } = req.body;
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+  if(!signature) res.status(404).send({ message: "signature dont was provide" });
+  if(!recovery) res.status(400).send({ message: "recovery dont was provide" });
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+  // setInitialBalance(sender);
+  // setInitialBalance(recipient);
+
+  // if (balances[sender] < amount) {
+  //   res.status(400).send({ message: "Not enough funds!" });
+  // } else {
+  //   balances[sender] -= amount;
+  //   balances[recipient] += amount;
+  //   res.send({ balance: balances[sender] });
+  // }
+  try {
+    
+    const bytes = utf8ToBytes(JSON.stringify({ sender, recipient, amount }));
+    const hash = keccak256(bytes);
+
+    const sig = new Uint8Array(signature);
+
+    const publicKey = await secp.recoverPublicKey(hash, sig, recovery);
+
+    if(toHex(publicKey) !== sender){
+      res.status(400).send({ message: "signature no is valid" });
+    }
+
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
+
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
+  } catch (error) {
+    console.log(error.message)
   }
 });
 
